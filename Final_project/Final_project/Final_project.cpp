0f9c1dd2903d9d6398f4f34eb85e6ce3cab593cc
HackLuigi <rubenjrnarro@gmail.com>
2025-05-14 10:56:26 -0600

    Proyecto final
    Introduccion

---
+++ Final_project/Final_project/Final_project.cpp
@@ -0,0 +1,50 @@
+#include <time.h>
+#include <string>
+#include <iostream>
+#include <windows.h>
+#include <locale>
+
+using namespace std;
+
+int main()
+{
+    string player;
+    const char* soundPath = "C:Users\ALUMNO\Desktop\Proyecto final\Nuclear_explosion.wav";
+
+    setlocale(LC_ALL, "es_MX.UTF-8");
+    cout << "Un año, paso un maldito y entero año..." << endl;
+    system("pause");
+    system("cls");
+    cout << "Desde que la vida de todo el planeta cambio por completo..." << endl;
+    system("pause");
+    system("cls");
+    cout << "No recuerdo mucho, no creo incluso recordar nada sobre mi, solo recuerdo mi nombre con mucha dificultad, el cual es..." << endl;
+    cin >> player;
+    cin.ignore();
+    system("cls");
+    cout << "Si... " << player << "..." << endl;
+    system("pause");
+    system("cls");
+    cout << "Es increible que todavia lo recuerde... Hoy en dia todos me llaman 020" << endl;
+    system("pause");
+    system("cls");
+    cout << "Todos tenemos esa clase de nombres por numeros, fuimos maldecidos por tener el 'privilegio' de poder meternos al bunker" << endl;
+    system("pause");
+    system("cls");
+    cout << "Solo me pregunto que sera del mundo afuera... Habra alguien vivo...? Me pongo triste de solo pensarlo..." << endl;
+    system("pause");
+    system("cls");
+    cout << "Hay veces en las que escucho disparos y pisadas gigantes de afuera, siempre me mata la curiosidad de saber que son..." << endl;
+    system("pause");
+    system("cls");
+    cout << "La comida en este pequeño lugar esta empezando a escacear, todos parecen estar mas territoriales que nunca..." << endl;
+    cout << "Y yo ya estoy harto de este lugar, es mi momento, ahora que todos estan en un drama serio, voy a salir de aqui de una u otra manera" << endl;
+    system("pause");
+    system("cls");
+    system("pause");
+    system("cls");
+    cout << "Despues de un largo dia, despiertas, preparado para empezar tu plan" << endl;
+    system("pause");
+    system("cls");
+    cout << "Que te gustaria hacer?" << endl;
+}
